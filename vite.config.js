5cfd314fdb1f7f2cee53b8e3735947a2e71c2329
ashwakshaik14 <ashwakshaik15@gmail.com>
2025-01-27 18:37:23 +0530

    Add allowedHosts to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,33 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import process from 'node:process';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0', // Bind to all network interfaces
+//     port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5173, // Use Render's `PORT` or fallback to 5173
+//   },
+// });
+
+
+
+
+
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import process from 'node:process';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Bind to all network interfaces
+    host: '0.0.0.0', // Allows binding to all network interfaces
     port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5173, // Use Render's `PORT` or fallback to 5173
+    allowedHosts: ['short-url-r755.onrender.com'], // Add your deployment host here
   },
 });
