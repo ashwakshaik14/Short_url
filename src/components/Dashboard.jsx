35eeaee39721786e181ade23425a036aea6ff7a9
ashwakshaik14 <ashwakshaik15@gmail.com>
2025-01-29 03:40:15 +0530

    dash

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -380,7 +380,7 @@ function Dashboard() {
   useEffect(() => {
     const dateLabels = dateWiseClicks.map((item) => item.date);
     const dateClicks = dateWiseClicks.map(
-      (item) => item.clicks
+      (item) => item.cumulativeClicks
     );
 
     setDateChartData({
